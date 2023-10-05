8a351fa1c38a8cd360a5eda5d6068b23f00ca8f4
Dell-User <Dell-User@DESKTOP-I2HH80B>
2023-10-05 05:57:58 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnLen = New Button()
+        SuspendLayout()
+        ' 
+        ' btnLen
+        ' 
+        btnLen.BackColor = SystemColors.ControlDark
+        btnLen.ForeColor = Color.Cyan
+        btnLen.Location = New Point(309, 146)
+        btnLen.Name = "btnLen"
+        btnLen.Size = New Size(112, 34)
+        btnLen.TabIndex = 0
+        btnLen.Text = "Len Function"
+        btnLen.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlDark
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnLen)
+        Name = "Form1"
+        Text = "Len Function"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnLen As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub btnLen_Click(sender As Object, e As EventArgs) Handles btnLen.Click
+        Dim Mytext As String
+        Mytext = "Visual Basic 2022"
+        MsgBox(Len(Mytext))
+    End Sub
+End Class
